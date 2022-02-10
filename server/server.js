dc73fb24d11cc8c4253e4bf01b477d6fa1899e73
GeTtygreen <94148141+GeTtygreen@users.noreply.github.com>
2022-02-10 15:38:09 -0600

    fin

--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-
-
 const app = express()
+
+
 
 app.use(express.json())
 app.use(cors())
 
 app.use(express.static('client'))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+
 app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
+try{
+
+}catch(error){
+    console.error(error)
+}
 
 
 const port = process.env.PORT || 5050
