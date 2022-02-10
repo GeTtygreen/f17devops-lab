8a18de9a36c8a422b4df7365e6ada8374a961e46
GeTtygreen <94148141+GeTtygreen@users.noreply.github.com>
2022-02-10 14:45:38 -0600

    updated server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
-const express = require('express');
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/',()=>{
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+
+const port = process.env.PORT || 5050
+app.listen(port,()=>console.log(`server running on `+ port))
