cfea1f31ce7ad1bece489ff61e935d3f2cf4ece5
GeTtygreen <94148141+GeTtygreen@users.noreply.github.com>
2022-02-10 22:54:32 -0600

    update

--- server/server.js
+++ server/server.js
@@ -1,39 +1,36 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
 
+app.use(express.json());
+app.use(cors());
 
+// app.use(express.static(path.join(__dirname,"../client")));
 
-app.use(express.json())
-app.use(cors())
-
-app.use(express.static('client'))
-
-// include and initialize the rollbar library with your access token
 var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
   accessToken: process.env.ROLLBAR_TOKEN,
   captureUncaught: true,
-  captureUnhandledRejections: true
+  captureUnhandledRejections: true,
 });
 
-// record a generic message and send it to Rollbar
-rollbar.log("Hello world!");
+
+// rollbar.log("Hello world!");
 
 
-app.get('/',(req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'))
-})
-app.get('/',(req, res) => {
-try{
-     messItUp();
+app.get("/", (req, res) => {
+    try {
+      messItUp();
+    } catch (err) {
+      console.log("NOW YOU KNOW YOURE WRONG" + err.stack);
+      rollbar.log("SEE DANIEL IT WORKS " + err.stack)
+    }
+  rollbar.info("someone IN YOUR BIZ ");
+  console.log('how is it going')
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
 
-}catch(err){
-    console.log("NOW YOU KNOW YOURE WRONG" + err.stack);
-}
-res.sendFile(path.join(__dirname, '../client/index.html'))
-})
-
-const port = process.env.PORT || 5050
-app.listen(port,()=>console.log(`server running on `+ port))
+const port = process.env.PORT || 5050;
+app.listen(port, () => console.log(`server running on ` + port));
